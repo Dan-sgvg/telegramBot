57b850e3d1ebad23c746d874e961ad9d08faf190
Dan-sgvg <73816800+Dan-sgvg@users.noreply.github.com>
2022-01-12 14:51:32 +0300

    основная логика(запрос сайту погоды, упращенное отображение информации пользователю), класс содержащий ответ сайта.

--- Program.cs
+++ Program.cs
@@ -5,34 +5,98 @@ using Telegram.Bot;
 using Telegram.Bot.Args;
 using Telegram.Bot.Types.Enums;
 using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace teleBot
 {
     internal class Program
     {
         private static TelegramBotClient _bot;
-        private static readonly string token = "5076163340:AAF4imWaonoSaWwaNB2L5WXWv0bwGqCNq_c";
-        static void Main(string[] args)
+        private static WeatherResponse WR;
+
+        //перенести в отдельный файл
+        private static readonly string token = ""; 
+        private static readonly string weatherSite = "https://api.openweathermap.org/data/2.5/weather";
+        private static readonly string weatherSiteAppID = ""; 
+        
+        static void _Main(string[] args)
         {
             _bot = new TelegramBotClient(token) { Timeout = TimeSpan.FromSeconds(30) };
             Console.WriteLine($"Привет, я {_bot.GetMeAsync().Result.FirstName} и я помогу тебе узнать погоду.");
-
-            _bot.OnMessage += _bot_OnMessage;
-
+            _bot.OnMessage += Bot_OnMessage;
             _bot.StartReceiving();
             Console.ReadLine();
             _bot.StopReceiving();
         }
 
-        private static async void _bot_OnMessage(object sender, MessageEventArgs e)
+        private static async void Bot_OnMessage(object sender, MessageEventArgs e)
         {
             var message = e.Message;
             if (message.Type == MessageType.Text)
             {
-                await _bot.SendTextMessageAsync(message.Chat.Id, message.Text);
                 Console.WriteLine(message.Text);
+                if (1 == 1) //если существует в citiesListRU
+                {
+                    await GetWeather(message.Text); 
+                    var answer = $"Температура в {WR.name}: {Math.Round(WR.main.temp)} °C"; // сделать расширенный ответ
+                    Console.WriteLine(answer);
+                    await _bot.SendTextMessageAsync(message.Chat.Id, answer);
+                }
+                else
+                    await _bot.SendTextMessageAsync(message.Chat.Id, "Город не найден. Ошибка в орфографии.");
             }
         }
+        public static async Task GetWeather(string cityName)
+        {
+            try
+            {
+                var url = $"{weatherSite}?q={cityName}&unit=metric&appid={weatherSiteAppID}&lang=ru";
+                var httpWebResponse = WebRequest.Create(url).GetResponse();
+                using var reader = new StreamReader(httpWebResponse.GetResponseStream());
+                var response = reader.ReadToEnd();
+                WR = JsonConvert.DeserializeObject<WeatherResponse>(response);
+            }
+            catch (WebException e)
+            {
+                Console.WriteLine(e);
+                return;
+            }
+        }
+        //перенести в отдельный файл
+        public class WeatherResponse
+        {
+            public Weather[] weather { get; set; } // id, параметры погоды, описание, иконка
+            public Main main { get; set; } // темп, ощущается, max, min, давление, влажность
+            public Wind wind { get; set; } // скорость, "наклон" ветра
+            public Clouds clouds { get; set; } //% облачности
+            public string name { get; set; } //город
+        }
+        public class Main
+        {
+            public float temp { get; set; }
+            public float feels_like { get; set; }
+            public float temp_min { get; set; }
+            public float temp_max { get; set; }
+            public int pressure { get; set; }
+            public int humidity { get; set; }
+        }
+        public class Wind
+        {
+            public float speed { get; set; }
+            public int deg { get; set; }
+        }
+        public class Clouds
+        {
+            public int all { get; set; }
+        }
+        public class Weather
+        {
+            public int id { get; set; }
+            public string main { get; set; }
+            public string description { get; set; }
+            public string icon { get; set; }
+        }
+
     }
 
 }
